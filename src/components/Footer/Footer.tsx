af5890c9c29a4a3cd1428e26367b24747733c054
Illya Avramenko <illyaavramenko309@gmail.com>
2025-07-23 13:04:43 +0300

    Remove FB icon in Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,8 @@
 import React, { FC } from 'react';
 import './Footer.css';
-import { FaFacebookF, FaInstagram } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
 
-const FacebookIcon = FaFacebookF as FC;
+// const FacebookIcon = FaFacebookF as FC;
 const InstagramIcon = FaInstagram as FC;
 const FB_LINK = process.env.REACT_APP_FB_LINK;
 const INST_LINK = process.env.REACT_APP_INST_LINK;
@@ -11,15 +11,15 @@ export const Footer: FC = () => {
   console.log(FB_LINK, INST_LINK)
   return (
     <footer className="footer">
-      <a
-        href={FB_LINK}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="footer__icon"
-        aria-label="Facebook"
-      >
-        <FacebookIcon />
-      </a>
+      {/*<a*/}
+      {/*  href={FB_LINK}*/}
+      {/*  target="_blank"*/}
+      {/*  rel="noopener noreferrer"*/}
+      {/*  className="footer__icon"*/}
+      {/*  aria-label="Facebook"*/}
+      {/*>*/}
+      {/*  <FacebookIcon />*/}
+      {/*</a>*/}
       <a
         href={INST_LINK}
         target="_blank"
